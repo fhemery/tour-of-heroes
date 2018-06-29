ab7d2e1f0b7e3957621fcf4449655cb570b9970d
fhemery <frederic.hemery@gmail.com>
2018-06-29 23:38:35 +0200

    add hero strength

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mr. Nice', strength: 67 },
+  { id: 12, name: 'Narco',  strength: 95 },
+  { id: 13, name: 'Bombasto', strength: 60 },
+  { id: 14, name: 'Celeritas', strength: 75 },
+  { id: 15, name: 'Magneta', strength: 12 },
+  { id: 16, name: 'RubberMan', strength: 48 },
+  { id: 17, name: 'Dynama',strength: 31 },
+  { id: 18, name: 'Dr IQ', strength: 57 },
+  { id: 19, name: 'Magma', strength: 89 },
+  { id: 20, name: 'Tornado', strength: 28 }
 ];
